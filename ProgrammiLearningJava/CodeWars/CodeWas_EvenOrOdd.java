cc6af35211028735f78e3a2e3831e4ba7b32855c
cesare120702 <foschini.cesare.giuseppe@gmail.com>
2025-06-15 18:48:15 +0200

    initEsercizi

---
+++ ProgrammiLearningJava/CodeWars/CodeWas_EvenOrOdd.java
@@ -0,0 +1,28 @@
+package ProgrammiLearningJava.CodeWars;
+
+public class CodeWas_EvenOrOdd {
+
+    /*
+     * https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+     * Create a function that takes an integer as an argument and returns "Even" for
+     * even numbers or "Odd" for odd numbers.
+     */
+
+    public static void main(String[] args) {
+
+        int number = 2;
+
+        System.out.println(evenOrOdd(number));
+
+
+    }
+
+    public static String evenOrOdd(int number) {
+
+if(number%2 == 0){
+    return "Even";
+}
+
+        return "Odd"; // Place code here;
+    }
+}
